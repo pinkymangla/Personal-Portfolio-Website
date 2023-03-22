1b380fbc259e7b7a296cfab3d43cf67b56ea3741
Pinky <pinkymangla45@gmail.com>
2023-03-22 17:07:00 +0530

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*========================typing animation========== */
+var typed=new Typed(".typing",{
+    strings:["","Web Designer","web Developer","Coder"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
